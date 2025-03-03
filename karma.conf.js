b57fe33c74c4feea10110ed2c3e23072d287f0b1
lion <uali03.alm@gmail.com>
2025-03-03 22:53:25 +0500

    Upd&Upg

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/u-testing-front'),
+      dir: require('path').join(__dirname, './coverage/utesting-front'),
       subdir: '.',
       reporters: [
         { type: 'html' },
